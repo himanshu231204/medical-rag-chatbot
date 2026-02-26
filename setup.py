a980f6126cdb2ea43228ed189687ffad666cb3b7
Himanshu kumar <himanshu231204@gmail.com>
2026-02-26 17:58:05 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_rag_chatbot',
+    version='0.1.0',
+    author='Himanshu kumar',
+    author_email='himanshu231204@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
